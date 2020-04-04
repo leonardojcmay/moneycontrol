49e2dc1f59e9dfb514c28293b498e9d50ec2a260
Leonardo May <leonardojcmay@hotmail.com>
2020-04-04 17:52:53 -0300

    Tela de cadastro finalizada

--- frontend/src/pages/NewSale/index.js
+++ frontend/src/pages/NewSale/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState  } from 'react';
 //Importando pacote, para que em toda rota não precisa recarregar toda a página e sim somente mudar de rota
 import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
@@ -19,7 +19,7 @@ export default function NewSale() {
     const [qtdParcela, setQtdParcela] = useState('');
     const [valorParcela, setValorParcela] = useState('');
     const [valorTotal, setValorTotal] = useState('');
-
+    
     //Pegando o id do user que esta logado
     const userId = localStorage.getItem('userId');
 
@@ -30,7 +30,7 @@ export default function NewSale() {
     async function handleNewSale(e) {
         e.preventDefault();
 
-        const dataC = {
+        const dataV = {
             loja,
             data,
             cartao,
@@ -41,7 +41,7 @@ export default function NewSale() {
         };
 
         try {
-            await api.post('sales', dataC, {
+            await api.post('sales', dataV, {
                 headers: {
                     Authorization: userId,//Verificando id do usuario logado
                 }
@@ -53,7 +53,24 @@ export default function NewSale() {
             alert('Erro ao cadastrar venda, tente novamente.');
         } 
     }
-    
+
+    // Função para deixar os campos de quantidade parcela e valor parcela em disabled caso não contenha parcelamento
+    // Wait for the page to finish loading
+    document.addEventListener('DOMContentLoaded', function () {
+    // Attach `change` event listener to checkbox
+        document.getElementById('billing-checkbox').onchange = toggleBilling;
+    }, false);
+  
+    function toggleBilling() {
+        // Select the billing text fields
+        var billingItems = document.querySelectorAll('#billing input[type="text"]');
+  
+        // Toggle the billing text fields
+        for (var i = 0; i < billingItems.length; i++) {
+        billingItems[i].disabled = !billingItems[i].disabled;
+        }
+    }
+
     return (
         <div className="new-sale-container">
             {/*div da parte de fora com a cor cinza claro */}
@@ -73,38 +90,58 @@ export default function NewSale() {
 
                 {/*Formulario de cadastro de vendas*/}
                 <form onSubmit={handleNewSale}>
+                    
+                    <div className="checkbox">
+                        <label>Loja 1</label>
+                        <input type="checkbox" value="Loja 1" style={{width:25, height:30}} onChange={e => setLoja(e.target.value)} />
+                            
+                        <label>Loja 2</label>
+                        <input type="checkbox" value="Loja 2" style={{width:25, height:30}} onChange={e => setLoja(e.target.value)}/>
+                    </div>
+
                     <input 
-                        placeholder="Loja" 
-                        value={loja}
-                        onChange={e => setLoja(e.target.value)}//onChange: ouvir as mudanças que ocorrem no input    
-                    />
-                    <textarea 
+                        type="date"
                         placeholder="Data" 
                         value={data}
                         onChange={e => setData(e.target.value)}
                     />
+
+                    <select value={cartao} onChange={e => setCartao(e.target.value)}>
+                        <option>Selecione o cartão</option>
+                        <option value="Getnet-Débito">Getnet-Débito</option>
+                        <option value="Getnet-Crédito">Getnet-Crédito</option>
+                        <option value="Banrisul">Banrisul</option>
+                    </select>
+
+                    <fieldset id="billing">
+                        <div className="checkbox">
+                            <label for="billing-checkbox">Venda parcelada?</label>
+                            
+                            <label>Sim</label>
+                            <input type="checkbox" value="Sim" style={{width:25, height:30}} onChange={e => setParcelamento(e.target.value)} id="billing-checkbox"/>
+                                
+                            <label>Não</label>
+                            <input type="checkbox" value="Não" style={{width:25, height:30}} onChange={e => setParcelamento(e.target.value)}/>
+                        </div>
+
+                        <input 
+                            disabled
+                            type="text"
+                            placeholder="Quantidade de parcelas" 
+                            value={qtdParcela}
+                            onChange={e => setQtdParcela(e.target.value)}//onChange: ouvir as mudanças que ocorrem no input    
+                        />
+                        <input 
+                            disabled
+                            type="text"
+                            placeholder="Valor da parcela" 
+                            value={valorParcela}
+                            onChange={e => setValorParcela(e.target.value)}
+                        />
+                    </fieldset>
+                    
                     <input 
-                        placeholder="Cartão" 
-                        value={cartao}
-                        onChange={e => setCartao(e.target.value)}//onChange: ouvir as mudanças que ocorrem no input    
-                    />
-                    <textarea 
-                        placeholder="Venda Parcelada?" 
-                        value={parcelamento}
-                        onChange={e => setParcelamento(e.target.value)}
-                    />
-                    <input 
-                        placeholder="Quantidade de Parcelas:" 
-                        value={qtdParcela}
-                        onChange={e => setQtdParcela(e.target.value)}//onChange: ouvir as mudanças que ocorrem no input    
-                    />
-                    <textarea 
-                        placeholder="Valor da parcela:" 
-                        value={valorParcela}
-                        onChange={e => setValorParcela(e.target.value)}
-                    />
-                    <input 
-                        placeholder="Valor Total:" 
+                        placeholder="Valor total" 
                         value={valorTotal}
                         onChange={e => setValorTotal(e.target.value)}
                     />
